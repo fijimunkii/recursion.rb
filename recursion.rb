5ca082885c66373549a17f8b6333493db1dcb35f
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:53:09 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -191,4 +191,14 @@ end
 
 
 
+# Given a string, compute recursively (no loops) a new string
+# where all appearances of "pi" have been replaced by "3.14".
+
+# changePi("xpix") → "x3.14x"
+# changePi("pipi") → "3.143.14"
+# changePi("pip") → "3.14p"
+
+
+
+
 
