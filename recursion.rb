97e8761444f89bbdf7fc755b3cc358d2953a93ce
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:49:07 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -93,3 +93,35 @@ end
 
 
 
+
+
+
+# Given a non-negative int n, return the sum of its digits
+# recursively (no loops). Note that mod (%) by 10 yields the
+# rightmost digit (126 % 10 is 6), while divide (/) by 10
+# removes the rightmost digit (126 / 10 is 12).
+
+# sumDigits(126) → 9
+# sumDigits(49) → 13
+# sumDigits(12) → 3
+
+
+
+
+
+
+# Given a non-negative int n, return the count of the occurrences
+# of 7 as a digit, so for example 717 yields 2. (no loops). Note
+# that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+# while divide (/) by 10 removes the rightmost digit
+# (126 / 10 is 12).
+
+# count7(717) → 2
+# count7(7) → 1
+# count7(123) → 0
+
+
+
+
+
+
