fa43090aa438a462c3f4b48926d7494601e605b8
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:33:52 -0500

    line breaks

--- recursion.rb
+++ recursion.rb
@@ -3,13 +3,19 @@ def sum(num)
   num + sum(num-1)
 end
 
-# Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+# Given n of 1 or more, return the factorial of n, which is
+# n * (n-1) * (n-2) ... 1. Compute the result recursively
+# (without loops).
 
 # factorial(1) → 1
 # factorial(2) → 2
 # factorial(3) → 6
 
-# Hint: First, detect the "base case", a case so simple that the answer can be returned immediately (here when n==1). Otherwise make a recursive call of factorial(n-1) (towards the base case). Assume the recursive call returns a correct value, and fix that value up to make our result.
+# Hint: First, detect the "base case", a case so simple that
+# the answer can be returned immediately (here when n==1).
+# Otherwise make a recursive call of factorial(n-1)
+# (towards the base case). Assume the recursive call returns
+# a correct value, and fix that value up to make our result.
 
 def factorial(num)
   return num if num == 1
@@ -17,13 +23,18 @@ def factorial(num)
 end
 
 
-# We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+# We have a number of bunnies and each bunny has two big floppy
+# ears. We want to compute the total number of ears across all
+# the bunnies recursively (without loops or multiplication).
 
 # bunnyEars(0) → 0
 # bunnyEars(1) → 2
 # bunnyEars(2) → 4
 
-# Hint: First detect the base case (bunnies == 0), and in that case just return 0. Otherwise, make a recursive call to bunnyEars(bunnies-1). Trust that the recursive call returns the correct value, and fix it up by adding 2.
+# Hint: First detect the base case (bunnies == 0), and in that
+# case just return 0. Otherwise, make a recursive call to
+# bunnyEars(bunnies-1). Trust that the recursive call returns
+# the correct value, and fix it up by adding 2.
 
 def bunnyEars(num)
   return num if num == 0
@@ -31,7 +42,14 @@ def bunnyEars(num)
 end
 
 
-# The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+# The fibonacci sequence is a famous bit of mathematics,
+# and it happens to have a recursive definition.
+# The first two values in the sequence are 0 and 1
+# (essentially 2 base cases). Each subsequent value is the
+# sum of the previous two values, so the whole sequence is:
+# 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive
+# fibonacci(n) method that returns the nth fibonacci number,
+# with n=0 representing the start of the sequence.
 
 # fibonacci(0) → 0
 # fibonacci(1) → 1
@@ -46,7 +64,12 @@ def fibonacci(num)
 end
 
 
-# We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+# We have bunnies standing in a line, numbered 1, 2, ...
+# The odd bunnies (1, 3, ..) have the normal 2 ears.
+# The even bunnies (2, 4, ..) we'll say have 3 ears,
+# because they each have a raised foot.
+# Recursively return the number of "ears" in the bunny line
+# 1, 2, ... n (without loops or multiplication).
 
 # bunnyEars2(0) → 0
 # bunnyEars2(1) → 2
