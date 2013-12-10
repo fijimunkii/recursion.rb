50a2c1b51d1cecb242f9db53210fcabd3f0e34c1
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:55:30 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -255,3 +255,12 @@ end
 
 
 
+
+# Given a string, compute recursively a new string where all
+# the adjacent chars are now separated by a "*".
+
+# allStar("hello") → "h*e*l*l*o"
+# allStar("abc") → "a*b*c"
+# allStar("ab") → "a*b"
+
+
