5dfcff8d5ebfedb0656c01972175bebdb708a029
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:55:51 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -264,3 +264,14 @@ end
 # allStar("ab") → "a*b"
 
 
+
+
+
+# Given a string, compute recursively a new string where
+# identical chars that are adjacent in the original string
+# are separated from each other by a "*".
+
+# pairStar("hello") → "hel*lo"
+# pairStar("xxyy") → "x*xy*y"
+# pairStar("aaaa") → "a*a*a*a"
+
