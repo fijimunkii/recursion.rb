67b1607716c655687185c85d3ad5d24b0fb81b84
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:58:59 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -333,3 +333,19 @@ end
 # stringClean("yyzzza") → "yza"
 # stringClean("abbbcdd") → "abcd"
 # stringClean("Hello") → "Helo"
+
+
+
+
+
+# Given a string, compute recursively the number of times
+# lowercase "hi" appears in the string, however do not count
+# "hi" that have an 'x' immedately before them.
+
+# countHi2("ahixhi") → 1
+# countHi2("ahibhi") → 2
+# countHi2("xhixhi") → 0
+
+
+
+
