c5bf5436c90a7eea2be7609b2d88317809b00143
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:52:08 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -176,3 +176,19 @@ end
 
 
 
+
+
+# Given a string, compute recursively (no loops) a new string
+# where all the lowercase 'x' chars have been changed to 'y' chars.
+
+# changeXY("codex") → "codey"
+# changeXY("xxhixx") → "yyhiyy"
+# changeXY("xhixhix") → "yhiyhiy"
+
+
+
+
+
+
+
+
