2729b84533c563c682e3b4ac3884f2a99cb7885e
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 15:53:28 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -202,3 +202,15 @@ end
 
 
 
+
+# Given a string, compute recursively a new string where all
+# the 'x' chars have been removed.
+
+# noX("xaxb") → "ab"
+# noX("abc") → "abc"
+# noX("xx") → ""
+
+
+
+
+
