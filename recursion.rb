464c8cf325895a6e103c40282ec87b5b46efbd62
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 16:05:47 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -491,3 +491,19 @@ end
 # groupSumClump(0, {2, 4, 8}, 10) → true
 # groupSumClump(0, {1, 2, 4, 8, 1}, 14) → true
 # groupSumClump(0, {2, 4, 4, 8}, 14) → false
+
+
+
+
+
+
+# Given an array of ints, is it possible to divide the ints into
+# two groups, so that the sums of the two groups are the same.
+# Every int must be in one group or the other. Write a recursive
+# helper method that takes whatever arguments you like, and make
+# the initial call to your recursive helper from splitArray().
+# (No loops needed.)
+
+# splitArray({2, 2}) → true
+# splitArray({2, 3}) → false
+# splitArray({5, 2, 3}) → true
