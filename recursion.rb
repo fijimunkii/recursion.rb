69fb266ad6200bb1c3c6dbed1fa93f29d19612a3
Harrison Powers <fijimunkii@gmail.com>
2013-12-10 16:01:52 -0500

    new exercise

--- recursion.rb
+++ recursion.rb
@@ -402,3 +402,15 @@ end
 
 
 
+# Given a string and a non-empty substring sub, compute
+# recursively the largest substring which starts and ends
+# with sub and return its length.
+
+# strDist("catcowcat", "cat") → 9
+# strDist("catcowcat", "cow") → 3
+# strDist("cccatcowcatxx", "cat") → 9
+
+
+
+
+
